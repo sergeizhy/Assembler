1f994d81a1f7cc518db60cf282de3c82c5bf7acb
Sergei Zhylin <serg.zhylin@gmail.com>
2021-10-04 01:25:30 +0300

    Update objdump.c

--- src/objdump.c
+++ src/objdump.c
@@ -1,20 +1,5 @@
 #include "headers/objdump.h"
 
-#define PRINT_DATA(container,address){                                                          \
-    size_t index;                                                                               \
-    container->curr_pos = 0;                                                                    \
-    while(container->curr_pos < container->size){                                               \
-        fprintf(ob,"%04d ",address += 4);                                                       \
-        for(index = 0; index < 4  && container->curr_pos < container->size; index++){           \
-            fprintf(ob,"%02X ",(char)container->table[container->curr_pos]->bits & 0xFF);       \
-            container->table[container->curr_pos]->bits >>= 8;                                  \
-            if(!--container->table[container->curr_pos]->size)                                  \
-                container->curr_pos++;                                                          \
-        }                                                                                       \
-        fprintf(ob,"\n");                                                                       \
-    }                                                                                           \
-}
-
 boolean objdump(IC* ic,DC* dc,char* base_file){
     uint32_t icf;
     uint32_t dcf;
